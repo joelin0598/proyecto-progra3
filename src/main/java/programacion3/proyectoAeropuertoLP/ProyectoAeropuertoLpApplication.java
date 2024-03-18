be32e346b57dba86486b0176d1dac88f1642232a
eJwtzF1rwjAUgOH/cq5z06YpNneCmwxFB5MxvAn5OHGhaRNOWp2I/30gvX3heR+grcVSTqnHESTYyouKV7xuRe0b1Nxg7UXnWlcJ0XXcOV6bxhhgkCldg0Na6GM5qWlZXX6Tev88lO1u8/NN41deD2uD+Xxo9+ePt2a7sjwfN74FBi+ipntGkGBQExIwKDa9gnZDGGWeTQxW9XhnhNrJRBdGmBObC5LEQYfIbol6H9MNGOBfDoRF6QnkOMf4fP4DL2FPwg== <joelmorales0598@gmail.com>
2024-03-17 21:48:15 -0600

    Commit inicial proyectoAeropuertoLP

---
+++ src/main/java/programacion3/proyectoAeropuertoLP/ProyectoAeropuertoLpApplication.java
@@ -0,0 +1,13 @@
+package programacion3.proyectoAeropuertoLP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAeropuertoLpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAeropuertoLpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programacion3/proyectoAeropuertoLP/ProyectoAeropuertoLpApplicationTests.java
@@ -0,0 +1,13 @@
+package programacion3.proyectoAeropuertoLP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAeropuertoLpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
