2987efd62980383de7aa3300ac62dd2ff3f1263b
eJwtzF1rwjAUgOH/cq5z06YpNneCmwxFB5MxvAn5OHGhaRNOWp2I/30gvX3heR+grcVSTqnHESTYyouKV7xuRe0b1Nxg7UXnWlcJ0XXcOV6bxhhgkCldg0Na6GM5qWlZXX6Tev88lO1u8/NN41deD2uD+Xxo9+ePt2a7sjwfN74FBi+ipntGkGBQExIwKDa9gnZDGGWeTQxW9XhnhNrJRBdGmBObC5LEQYfIbol6H9MNGOBfDoRF6QnkOMf4fP4DL2FPwg== <joelmorales0598@gmail.com>
2024-03-31 17:32:50 -0600

    Entidades creadas con mapeos

---
+++ src/main/java/programacion3/proyectoAeropuertoLP/model/entity/AsignacionTV.java
@@ -0,0 +1,29 @@
+package programacion3.proyectoAeropuertoLP.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "tripulacion_vuelo")
+public class AsignacionTV {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @JsonIgnore
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinColumn(name = "tripulacion_id",referencedColumnName = "id")
+    private Tripulacion tripulacionId;
+
+    @JsonIgnore
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "numero_vuelo_id",referencedColumnName = "id")
+    private Vuelo numeroVueloId;
+}
